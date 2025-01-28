e2454c94b525f0060fb3901aaca785c083e43b8a
Ellu <alikoskieveliina@gmail.com>
2025-01-28 19:43:22 +0200

    tehtävät 2b

---
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -0,0 +1,14 @@
+const Persons = ({ persons }) => {
+    return (
+        persons.map(person => (
+            <Person key={person.id} person={person} />
+        ))
+    )
+}
+
+const Person = ({person}) => {
+    return (
+      <p>{person.name} {person.number}</p>
+    )}
+
+export default Persons

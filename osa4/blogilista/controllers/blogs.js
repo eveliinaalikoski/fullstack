d58234794f1b572c4573ac545a5547a144427645
Ellu <alikoskieveliina@gmail.com>
2025-05-19 22:17:06 +0300

    teht 4.13-14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -17,6 +18,32 @@ blogsRouter.post('/', async (request, response) => {
 
 	const savedBlog = await blog.save()
 	response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+	const deletion = await Blog.findByIdAndDelete(request.params.id)
+
+	if (deletion) {
+		response.status(204).end()
+	} else {
+		response.status(400).end()
+	}
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+	const { title, author, url, likes } = request.body
+
+	const updatedBlog = await Blog.findByIdAndUpdate(
+		request.params.id,
+		{ title, author, url, likes },
+		{ new: true }
+	)
+
+	if (updatedBlog) {
+		response.json(updatedBlog)
+	} else {
+		response.status(400).end()
+	}
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -35,84 +35,150 @@ test('indentifying field for blogs is id', async () => {
 		assert.strictEqual(blog._id, undefined)
 	}
 })
-
-test('a valid blog can be added', async () => {
-	const newBlog = {
-    id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  }
-
-	await api
-		.post('/api/blogs')
-		.send(newBlog)
-		.expect(201)
-    .expect('Content-Type', /application\/json/)
-
-	const response = await api.get('/api/blogs')
-	const titles = response.body.map(r => r.title)
-
-	assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
-	assert(titles.includes(newBlog.title))
+describe('addition of a new blog', () => {
+	test('a valid blog can be added', async () => {
+		const newBlog = {
+			id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+			likes: 12,
+	  }
+	
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+	
+		const response = await api.get('/api/blogs')
+		const titles = response.body.map(r => r.title)
+	
+		assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
+		assert(titles.includes(newBlog.title))
+	})
+	
+	test('if not given a value, field likes gets value zero', async () => {
+		const newBlog = {
+			id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+	  }
+	
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+	
+		const response = await api.get('/api/blogs')
+		const likes = response.body.map(r => r.likes)
+	
+		assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
+		assert(likes.includes(0))
+	})
+	
+	test('can not add a blog without title', async () => {
+		const newBlog = {
+			id: "5a422b3a1b54a676234d17f9",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+			likes: 12,
+	  }
+	
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(400)
+	
+		const response = await api.get('/api/blogs')
+	
+		assert.strictEqual(response.body.length, helper.initialBlogs.length)
+	})
+	
+	test('can not add a blog without url', async () => {
+		const newBlog = {
+			id: "5a422b3a1b54a676234d17f9",
+			title: "Canonical string reduction",
+			author: "Edsger W. Dijkstra",
+			likes: 12,
+	  }
+	
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(400)
+	
+		const response = await api.get('/api/blogs')
+	
+		assert.strictEqual(response.body.length, helper.initialBlogs.length)
+	})	
 })
 
-test('if not given a value, field likes gets value zero', async () => {
-	const newBlog = {
-    id: "5a422b3a1b54a676234d17f9",
-    title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-  }
+describe('deletion of a blog', () => {
+	test('succeeds with status code 204 if id is valid', async () => {
+		const blogsAtStart = await helper.blogsInDb()
+		const blogToDelete = blogsAtStart[0]
 
-	await api
-		.post('/api/blogs')
-		.send(newBlog)
-		.expect(201)
-    .expect('Content-Type', /application\/json/)
+		await api
+			.delete(`/api/blogs/${blogToDelete.id}`)
+			.expect(204)
+		
+		const blogsAtEnd = await helper.blogsInDb()
+		const ids = blogsAtEnd.map(n => n.id)
+		
+		assert(!ids.includes(blogToDelete.id))
+		assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length-1)
+	})
 
-	const response = await api.get('/api/blogs')
-	const likes = response.body.map(r => r.likes)
+	test('fails with status code 400 if id is invalid', async () => {
+		const invalidId = '5a422a851b54a676234d17f7'
 
-	assert.strictEqual(response.body.length, helper.initialBlogs.length+1)
-	assert(likes.includes(0))
+		await api
+			.delete(`/api/blogs/${invalidId}`)
+			.expect(400)
+		
+		const blogsAtEnd = await helper.blogsInDb()
+		
+		assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+	})
 })
 
-test('can not add a blog without title', async () => {
-	const newBlog = {
-    id: "5a422b3a1b54a676234d17f9",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-    likes: 12,
-  }
+describe('edition of a blog', () => {
+	test('succeeds if id is valid', async () => {
+		const blogsAtStart = await helper.blogsInDb()
+		const blogToUpdate = blogsAtStart[0]
 
-	await api
-		.post('/api/blogs')
-		.send(newBlog)
-		.expect(400)
+		const updatedBlog = { likes: 12 }
 
-	const response = await api.get('/api/blogs')
+		await api
+			.put(`/api/blogs/${blogToUpdate.id}`)
+			.send(updatedBlog)
+			.expect(200)
+		
+		const blogsAtEnd = await helper.blogsInDb()
+		const likes = blogsAtEnd.map(n => n.likes)
 
-	assert.strictEqual(response.body.length, helper.initialBlogs.length)
-})
+		assert(!likes.includes(blogToUpdate.likes))
+		assert(likes.includes(updatedBlog.likes))
+	})
 
-test('can not add a blog without url', async () => {
-	const newBlog = {
-    id: "5a422b3a1b54a676234d17f9",
-		title: "Canonical string reduction",
-    author: "Edsger W. Dijkstra",
-    likes: 12,
-  }
+	test('fails with status code 400 if id is invalid', async () => {
+		const invalidId = '5a422a851b54a676234d17f7'
+		const updatedBlog = { likes: 12 }
 
-	await api
-		.post('/api/blogs')
-		.send(newBlog)
-		.expect(400)
+		await api
+			.put(`/api/blogs/${invalidId}`)
+			.send(updatedBlog)
+			.expect(400)
+		
+		const blogsAtEnd = await helper.blogsInDb()
+		const likes = blogsAtEnd.map(n => n.likes)
 
-	const response = await api.get('/api/blogs')
-
-	assert.strictEqual(response.body.length, helper.initialBlogs.length)
+		assert(!likes.includes(updatedBlog.likes))
+		assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+	})
 })
 
 after(async () => {
